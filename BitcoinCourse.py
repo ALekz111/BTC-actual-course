cd5d500a846ee9cddc124baee4aaa3711745c20b
ALekz111 <157841614+ALekz111@users.noreply.github.com>
2024-12-10 19:46:30 +0300

    Add files via upload

--- BitcoinCourse.py
+++ BitcoinCourse.py
@@ -6,12 +6,18 @@ header = {
     'user-agent': 'Slinux'
 }
 
+BTC = 0
 link = 'https://www.okx.com/ru/price/bitcoin-btc'
+
 while True:
     response = requests.get(link, headers=header).text
     soup = BeautifulSoup(response, 'lxml')
     block = soup.find('div', id="price-detail-header")
 
     bitcoin_course = block.find_all('div')[0].text
-    print("Актуальный курс биткоина:",bitcoin_course)
-    time.sleep(60)
+
+    if bitcoin_course != BTC:
+        BTC = bitcoin_course
+        print(BTC)
+    else:
+        continue
